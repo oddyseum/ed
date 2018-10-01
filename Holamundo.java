265795dd6174f29eb41a2bb166677c5242330793
oddyseum <threshpain@hotmail.com>
2018-10-01 09:33:15 +0200

    Añadido JHolaMundo con eclipse

---
+++ Holamundo.java
@@ -0,0 +1,6 @@
+public class Holamundo {
+	
+	public static void main(String[] args)
+{
+	System.out.printIn("Hola mundo desde java");
+}
---
+++ JHolaMundo/src/org/institutoserpis/ed/JHolaMundo.java
@@ -0,0 +1,11 @@
+package org.institutoserpis.ed;
+
+public class JHolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo desde Package");
+		// TODO Auto-generated method stub
+
+	}
+
+}
